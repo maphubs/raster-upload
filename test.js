9e0f046fa7a3dac762f578e9ebd29ef8896d1d9c
Kristofor Carle <kris@maphubs.com>
2020-11-11 23:39:02 -0500

    support vector tile MBTiles uploads

--- test.js
+++ test.js
@@ -1,10 +1,9 @@
 require('@babel/register')
 const tiles = require('./lib/tiles')
 
-const input = './data/test.mbtiles'
-const output = 's1test'
+const input = './data/out.mbtiles'
 
-tiles.createTilesMBTiles(input, output).then((tileJSON) => {
+tiles.createTilesMBTiles(input).then((tileJSON) => {
   console.log(tileJSON)
   console.log('FINISHED')
   process.exit(1) // eslint-disable-line unicorn/no-process-exit
